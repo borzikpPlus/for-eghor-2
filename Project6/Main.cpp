06cea340d57e42e90fb7146dc2288c9bbbfc38bf
Borza_qt35@student.itstep.org <Borza_qt35@student.itstep.org>
2025-09-10 13:17:52 +0300

    Add project files.

---
+++ Project6/Main.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using namespace std;
+
+class CPU
+{
+private:
+	char* model;
+	double power;
+	double price;
+
+public:
+	CPU():model(nullptr),power(0),price(0)
+	{}
+	CPU(const char* mod, double pow, double pr) : power(pow), price(pr)
+	{
+		model = new char[strlen(mod) + 1];
+		strcpy_s(model, strlen(mod) + 1, mod);
+	}
+	~CPU ()
+	{
+		delete[]model;
+	}
+	void Print()
+	{
+		cout << "--- CPU: \n";
+		cout << model << "\t" << power << "\t" << price << endl;
+	}
+};
+
+
+
+
+
+class PC
+{
+private:
+	char* model;
+	double price;
+	CPU Cpu;
+public:
+	PC(const char* mod, double pr, const char* cp, double pcpu, double prcpu) : price(pr), Cpu(cp, pcpu, prcpu)
+	{
+		model = new char[strlen(mod) + 1];
+		strcpy_s(model, strlen(mod) + 1, mod);
+	}
+
+
+
+
+
+
+
+};
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+	return 0;
+
+
+}
