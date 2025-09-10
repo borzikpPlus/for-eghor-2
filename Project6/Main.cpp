b5354316df8567ee74822d9c6bbcb5baa12a614e
Borza_qt35@student.itstep.org <Borza_qt35@student.itstep.org>
2025-09-10 14:06:16 +0300

    123

--- Project6/Main.cpp
+++ Project6/Main.cpp
@@ -2,61 +2,25 @@
 
 using namespace std;
 
-class CPU
+
+
+
+class Elevator
 {
 private:
-	char* model;
-	double power;
-	double price;
+
+	int direction;
+	bool on_off;
+	bool state_active;
+	int floor;
 
 public:
-	CPU():model(nullptr),power(0),price(0)
+	Elevator() :direction(0), floor(0)
 	{}
-	CPU(const char* mod, double pow, double pr) : power(pow), price(pr)
+	void Init(int direction, int floor, bool state_active, bool on_off)
 	{
-		model = new char[strlen(mod) + 1];
-		strcpy_s(model, strlen(mod) + 1, mod);
+
 	}
-	~CPU ()
-	{
-		delete[]model;
-	}
-	void Print()
-	{
-		cout << "--- CPU: \n";
-		cout << model << "\t" << power << "\t" << price << endl;
-	}
-};
-
-
-
-
-
-class PC
-{
-private:
-	char* model;
-	double price;
-	CPU Cpu;
-public:
-	PC(const char* mod, double pr, const char* cp, double pcpu, double prcpu) : price(pr), Cpu(cp, pcpu, prcpu)
-	{
-		model = new char[strlen(mod) + 1];
-		strcpy_s(model, strlen(mod) + 1, mod);
-	}
-
-	~PC()
-	{
-		delete[] model;
-	}
-
-	void Print()
-	{
-		cout << "--- CPU: \n";
-		cout << model << "\t" << price << "\t" << price << endl;
-		Cpu.Print();
-	}
-
 
 
 };
@@ -67,10 +31,6 @@ public:
 
 int main()
 {
-	PC obj("HP", 2000, "Amd", 3.5, 9000);
-	obj.Print();
-
-
 
 
 
